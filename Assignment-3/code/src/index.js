c00fb4a72fa89d316e529e0369f02640a94cc338
souravq <souravbera515@gmail.com>
2022-02-07 16:05:07 +0530

    React Third Assignment Done

--- Assignment-3/code/src/index.js
+++ Assignment-3/code/src/index.js
@@ -4,9 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const employee = {
+  name: 'Sourav Bera',
+  description: 'If you want to start measuring performance in your app, pass a function',
+  appoinment: '9:30 A.M 08:02:2022',
+  email:'sample@gmail.com',
+  phone:'+919807654321',
+  status:'Active',
+  door:'Mark',
+  time:'11.00 A.M',
+  profileImg: 'https://www.w3schools.com/howto/img_avatar.png'
+}
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App employee={employee} />
   </React.StrictMode>,
   document.getElementById('root')
 );
